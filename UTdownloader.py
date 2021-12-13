6a1897736efe08b742ed755ad62bd63363be1fca
razmarinzer <razmarinzer@mail.ru>
2021-12-13 18:58:17 +0300

    initial commit

---
+++ UTdownloader.py
@@ -0,0 +1,43 @@
+
+""" Cуть программы: Написать скрипт, который ищет новые видео на youtube на канале selfedu и загружает их в каталог
+на локальный компьютер. Новые видео - те, которые еще не были загружены при предыдущих запусках скрипта. Предусмотреть
+ запуск скрипта по расписанию. Предусмотреть запись ошибок в текстовый файл или базу данных. Предусмотреть
+ дополнительный метод для очистки списка уже загруженных файлов. Хранить список загруженных видео в текстовом
+ файле или в базе данных. В будущем можно сделать интерфейс для интерактивной работы. Интерфейс - окно, кнопка
+  - прочитать список, читает список всех видео на канале, отмечает уже загруженные. Вторая кнопка “загрузить”
+   - загружает видео, отмеченные в списке для загрузки."""
+
+
+class UTdownloader:
+
+    def __init__(self):
+        self.db_controller = DBManager()
+
+    def download_all_new(self):
+        new_video_refs = self.get_new_video_refs()
+        for video_ref in new_video_refs:
+            self.download_video_from_ref(video_ref)
+
+    def get_new_video_refs(self):
+        old_video_refs = self.db_controller.get_old_video_refs()
+
+        return []
+
+    def download_video_from_ref(self, video_ref):
+        pass
+
+
+class Parser:
+    
+    pass
+
+class DBManager:
+
+    def get_old_video_refs(self):
+        return []
+
+
+if __name__ == '__main__':
+
+    downloader = UTdownloader()
+    result = downloader.download_all_new()
