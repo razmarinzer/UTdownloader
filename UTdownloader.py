c64abbbdb2909853a269e77a7f6364a70d1b5cae
razmarinzer <razmarinzer@mail.ru>
2022-01-08 15:42:41 +0300

    programm tests, full video download, bug fixes.

--- UTdownloader.py
+++ UTdownloader.py
@@ -2,8 +2,8 @@ from lxml import html
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import os
 
+import os
 import time
 
 import pytube
@@ -20,26 +20,35 @@ import moviepy.editor as mpe
 
 class UTDownloader:
 
-    def __init__(self):
-        self.db_controller = DBManager()
+    def __init__(self, folder=''):
+
+        self.folder = folder or 'Youtube_videos'
+        if not os.path.isdir(self.folder):
+            os.mkdir(self.folder)
+
+        self.db_controller = DBManager(self.folder)
         self.parser = Parser('selfedu_rus')
-        self.downloader = Downloader()
+        self.downloader = Downloader(self.folder)
 
     def download_all_new(self):
-        new_video_refs = self.get_new_video_refs()
-        for video_ref in new_video_refs:
-            self.downloader.download_video(video_ref)
+        new_video_id = self.get_new_video_id()
+        print('Downloading started-------------')
+        for video_id in new_video_id:
+            self.downloader.download_video(video_id) #, with_higest_res=True, merge_with_audio=True)
+            print('--- {} dowloaded'.format(video_id))
+        print('Downloading finished------------')
 
-        return new_video_refs
+        return 'Ok'
 
-    def get_new_video_refs(self):
-        old_video_refs = self.db_controller.get_old_video_refs()
-        all_video_refs = self.parser.get_all_video_refs()
+    def get_new_video_id(self):
+        old_video_id = self.db_controller.get_old_video_id()
+        all_video_id = self.parser.get_all_video_id()
 
-        new_video_refs = [element for element in all_video_refs if element not in old_video_refs]
+        new_video_id = [element for element in all_video_id if element not in old_video_id]
 
-        new_video_refs = ['https://www.youtube.com/watch?v=bN1xpI6f3Vo']
-        return new_video_refs
+        # new_video_id = ['bN1xpI6f3Vo', 'Kmiw4FYTg2U']
+        print(old_video_id)
+        return new_video_id
 
 
 class Parser:
@@ -49,20 +58,23 @@ class Parser:
         self._main_link = 'https://www.youtube.com/'
         self._scroll_pause_time = scroll_pause_time or 3
 
-    def get_all_video_refs(self):
-
+    def get_all_video_id(self):
+        print('Parsing started ---------------')
+        print('Reading channel html-----------')
         html_text = self._get_html_text()
-
+        print('Html readed-----------')
+        print('Parsing id from html---------')
         root = html.fromstring(html_text)
 
         elements = root.xpath("//h3[@class='style-scope ytd-grid-video-renderer']/a/@href")
 
-        elements = [self._main_link + el for el in elements]
+        elements = [el.split('=')[-1] for el in elements]
+        print('{} elements found-------------'.format(len(elements)))
+        print('Parsing finished --------------')
 
         return elements
 
     def _get_html_text(self):
-        pass
 
         options = Options()
         options.add_argument('--headless')
@@ -72,7 +84,8 @@ class Parser:
         driver.get('{}/c/{}/videos'.format(self._main_link, self.channel))
 
         last_height = driver.execute_script("return document.documentElement.scrollHeight")
-        print('Current height - {}'.format(last_height))
+        print('\r', end='')
+        print('Current html height {} px'.format(last_height), end='')
         while True:
             # Scroll down to bottom
             driver.execute_script("window.scrollTo(0,document.documentElement.scrollHeight);")
@@ -82,11 +95,13 @@ class Parser:
 
             # Calculate new scroll height and compare with last scroll height
             new_height = driver.execute_script("return document.documentElement.scrollHeight")
-            print('Current height - {}'.format(new_height))
+            print('\r', end='')
+            print('Current html height {} px'.format(new_height), end='')
             if new_height == last_height:
-                print("Thats enough")
                 break
             last_height = new_height
+
+        print()
 
         html_text = driver.page_source
 
@@ -97,29 +112,40 @@ class Parser:
 
 class DBManager:
 
-    def get_old_video_refs(self):
-        return []
+    def __init__(self, folder):
+        self.folder = folder
+
+    def get_old_video_id(self):
+
+        file_list = os.listdir(self.folder)
+        id_list = []
+        for filename in file_list:
+            filename_list = filename.split(' - ')
+            video_id = filename_list[0]
+            id_list.append(video_id)
+
+        return list(set(id_list))
 
 
 class Downloader:
 
-    def __init__(self, folder=''):
-        self.folder = folder or 'Youtube_videos'
-        if not os.path.isdir(folder):
-            os.mkdir(folder)
-
+    def __init__(self, folder):
+        self.folder = folder
+        self.ref_part = 'https://www.youtube.com/watch?v='
         self.title = ''
 
-    def download_video(self, ref, with_higest_res=False, merge_with_audio=False):
+    def download_video(self, id, with_higest_res=False, merge_with_audio=False):
+
+        ref = self.ref_part + id
 
         you_video = pytube.YouTube(ref)
 
         video_streams = you_video.streams.filter(subtype="mp4")
 
         if not with_higest_res:
-            video_streams.filter(progressive=True)
+            video_streams = video_streams.filter(progressive=True)
 
-        video_streams.order_by('resolution').desc()
+        video_streams = video_streams.order_by('resolution').desc()
 
         if not video_streams:
             raise IndexError('Video streams is empty')
@@ -138,7 +164,12 @@ class Downloader:
 
         filename = you_video.video_id + ' - ' + you_video.title
 
-        if not video_stream.is_progressive:
+        # tabu_symb_list = ['<', '>', ':', '/', '\\', '|', '?', '*']
+
+        for depr_symb in '<>:/\\|?*':
+            filename = filename.replace(depr_symb, '')
+
+        if video_stream.is_progressive:
             video_stream.download(output_path=self.folder, filename=filename + '.mp4')
         else:
             video_stream.download(output_path=self.folder, filename=filename + ' - video.mp4')
@@ -158,12 +189,11 @@ class Downloader:
         final_filename = ''
         for filename in file_list:
             namelist = filename.split(' - ')
-            if len(namelist) == 3:
-                if namelist[2] == 'video.mp4':
-                    filename_video = filename
-                    final_filename = namelist[0] + namelist[1] + '.mp4'
-                elif namelist[2] == 'audio.webm':
-                    filename_audio = filename
+            if namelist[-1] == 'video.mp4':
+                filename_video = filename
+                final_filename = ' - '.join(namelist[:-1]) + '.mp4'
+            elif namelist[-1] == 'audio.webm':
+                filename_audio = filename
 
         if not filename_video:
             raise FileNotFoundError('Video file not found')
@@ -171,11 +201,18 @@ class Downloader:
         if not filename_audio:
             raise FileNotFoundError('Audio file not found')
 
+        filename_video = os.path.join(self.folder, filename_video)
+        filename_audio = os.path.join(self.folder, filename_audio)
+        final_filename = os.path.join(self.folder, final_filename)
+
         my_clip = mpe.VideoFileClip(filename_video)
         final_audio = mpe.AudioFileClip(filename_audio)
 
         final_clip = my_clip.set_audio(final_audio)
         final_clip.write_videofile(final_filename)
+
+        os.remove(filename_video)
+        os.remove(filename_audio)
 
     @staticmethod
     def _get_resolution_value(resolution_string):
@@ -185,6 +222,7 @@ class Downloader:
     def _get_video_id_from_filename(filename):
         namelist = filename.split(' - ')
         return namelist[0]
+
 
 if __name__ == '__main__':
     downloader = UTDownloader()
